639481d064a33ec3cb9b6aac06ba6148302bf752
Ricardo Gonçalves <ricardo.341928@hotmail.com>
2023-12-01 15:25:23 +0000

    add filter for folders

--- hooks.lua
+++ hooks.lua
@@ -1,9 +1,17 @@
 local files = {"templater.yaml", "hooks.lua", ".gitignore"}
+local folders = {".git/"}
 
 function keepFile(path)
     for _, value in ipairs(files)
     do
        if (path == value) then
+            return false
+       end
+    end
+
+    for _, value in ipairs(folders)
+    do
+       if (self:sub(1, #start) == start) then
             return false
        end
     end
